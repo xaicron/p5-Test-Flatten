aa34923e6132b733d2783594760b84128757721a
xaicron <xaicron@gmail.com>
2011-07-30 03:06:59 +0900

    initial commit

---
+++ lib/Test/Flatten.pm
@@ -0,0 +1,42 @@
+package Test::Flatten;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Test::Flatten -
+
+=head1 SYNOPSIS
+
+  use Test::Flatten;
+
+=head1 DESCRIPTION
+
+Test::Flatten is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
