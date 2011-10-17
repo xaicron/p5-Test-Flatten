d27622af87c871a2c15a9444dd6f14376518bfac
xaicron <xaicron@gmail.com>
2011-10-18 01:18:44 +0900

    Checking in changes prior to tagging of version 0.05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1d33873..2db43e0 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Test::Flatten
    
    +0.05  Tue Oct 18 01:10:44 2011
    +    - Added support for SUBTEST_FILTER environment variable. (idea from tokuhirom++)
    +
     0.04  Fri Sep 23 16:05:17 2011
         - fix: plan() did not works.

--- lib/Test/Flatten.pm
+++ lib/Test/Flatten.pm
@@ -6,7 +6,7 @@ use Test::More ();
 use Test::Builder ();
 use Term::ANSIColor qw(colored);
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 our $BORDER_COLOR  = [qw|cyan bold|];
 our $BORDER_CHAR   = '-';
