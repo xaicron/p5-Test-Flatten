3ad1aaa113b3ac606734350d0f51b68b6b191b18
xaicron <xaicron@gmail.com>
2011-12-27 01:39:49 +0900

    Checking in changes prior to tagging of version 0.07.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 7d5edaa..7e033a9 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Test::Flatten
    
    +0.07  Tue Dec 27 01:38:07 2011
    +    - fix: local $@ problem on less than 5.14.0 (reported by issm++)
    +
     0.06  Mon Nov  7 22:15:16 2011
         - fix: $Test::Builder::Level was wrong

--- lib/Test/Flatten.pm
+++ lib/Test/Flatten.pm
@@ -6,7 +6,7 @@ use Test::More ();
 use Test::Builder ();
 use Term::ANSIColor qw(colored);
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 our $BORDER_COLOR  = [qw|cyan bold|];
 our $BORDER_CHAR   = '-';
